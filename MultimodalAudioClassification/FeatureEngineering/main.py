327dba825c899cb74d04a7c7fad4a163b96e8d7d
LandonBuell <42503615+landonbuell@users.noreply.github.com>
2022-09-10 14:03:07 -0400

    Added FeatureEngineering Project

---
+++ MultimodalAudioClassification/FeatureEngineering/main.py
@@ -0,0 +1,28 @@
+"""
+Repo:       MultiModalAudioClassification
+Solution:   MultiModalAudioClassification
+Project:    FeautureEngineering
+File:       main.py
+
+Author:     Landon Buell
+Date:       Sept 2022
+"""
+
+    #### IMPORTS ####
+
+import sys
+
+import PyToolsStructures
+
+    #### MAIN EXECUTABLE ####
+
+if __name__ == "__main__":
+
+    # Set some constants + Load Run Info
+    RUN_PATH = "C:\Users\lando\Documents\audioFeatures\simpleSignalsV1"
+    runInfo = PyToolsStructures.RunInfo.deserialize(RUN_PATH)
+
+
+
+    # Exit App
+    sys.exit(0)
