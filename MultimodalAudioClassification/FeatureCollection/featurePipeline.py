1b55b5338e098e376b0208b799eda8372581ab9c
LandonBuell <42503615+landonbuell@users.noreply.github.com>
2024-02-05 18:49:27 -0500

    Added feature pipeline

---
+++ MultimodalAudioClassification/FeatureCollection/featurePipeline.py
@@ -0,0 +1,14 @@
+"""
+    Repo:       MultiModalAudioClassification
+    Solution:   MultiModalAudioClassification
+    Project:    FeautureCollection
+    File:       featurePipeline.py
+    Classes:    FeaturePipeline
+
+    Author:     Landon Buell
+    Date:       February 2024
+"""
+
+        #### CLASS DEFINTIONS ####
+
+class FeaturePipeline:
