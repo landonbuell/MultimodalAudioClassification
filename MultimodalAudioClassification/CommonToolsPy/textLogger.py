1ef03a2deb6116d1b36ebb12edafd2a0fed9f4ea
LandonBuell <42503615+landonbuell@users.noreply.github.com>
2024-02-05 21:35:59 -0500

    Added text logger

---
+++ MultimodalAudioClassification/CommonToolsPy/textLogger.py
@@ -0,0 +1,71 @@
+"""
+    Repo:       MultiModalAudioClassification
+    Solution:   MultiModalAudioClassification
+    Project:    CommonToolsPy
+    File:       textLogger.py
+    Classes:    TextLogger
+
+    Author:     Landon Buell
+    Date:       February 2024
+"""
+
+        #### IMPORTS ####
+
+import os
+import datetime
+
+        #### CLASS DEFINITIONS ####
+
+class TextLogger:
+    """ Represents a text logger """
+
+    def __init__(self,
+                 outputPath: str,
+                 toConsole=True):
+        """ Constructor """
+        self._outputPath = os.path.abspath(outputPath)
+        self._logCounter = 0
+        self._logToConsole = toConsole
+       
+    def __del__(self):
+        """ Destructor """
+        pass
+
+    # Accessors
+
+    def getOutputPath(self) -> str:
+        """ Return logger output path """
+        return self._outputPath
+
+    def getLogToConsole(self) -> bool:
+        """ Return T/F if we should log to console """
+        return self._logToConsole
+
+    # Public Interface
+    
+    def logMessage(self,
+                     message: str) -> None:
+        """ Log a message """
+        timeStamp = TextLogger.__getCurrentTimeStamp()
+        formattedMessage = "\t{0<8}{1:<24}{2}".format(self._logCounter,timeStamp,message)
+        if (self._logToConsole):
+            print(formattedMessage)
+        self.__writeToFile(formattedMessage)
+        return None
+
+    # Private Interface
+
+    def __writeToFile(self,message:str) -> None:
+        """ Write a message to the output file """
+        with open(self._outputPat,"a") as outputStream:
+            outputStream.write(message + "\n")
+        return None
+
+    @staticmethod
+    def __getCurrentTimeStamp():
+        """ Get the current time in YYYY.MM.DD.HH.MM.SS.UUUUUU """
+        now = datetime.datetime.now()
+        return str(now)
+
+
+
